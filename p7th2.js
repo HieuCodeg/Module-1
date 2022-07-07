53de1eecedcad1ab4d5fb3184c6dfa588d4a46d3
HieuCodeg <108601145+HieuCodeg@users.noreply.github.com>
2022-07-07 09:05:09 +0700

    noi dung

---
+++ p7th2.js
@@ -0,0 +1,6 @@
+inputwidth = prompt("Nhap chieu rong =");
+inputheight = prompt("Nhap chieu cao =");
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width * height;
+document.write("Diện tích là: " + area);
