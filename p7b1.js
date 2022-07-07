9731c870e47cae6299fe179652fb0b8e483158d0
HieuCodeg <108601145+HieuCodeg@users.noreply.github.com>
2022-07-07 08:49:32 +0700

    phan 7

---
+++ p7b1.js
@@ -0,0 +1,12 @@
+let i=10
+let f=20.5
+let b=true
+let s='Hà Nội'
+document.write('i = '+ i)
+document.write('<br/>')
+document.write('f = '+ f)
+document.write('<br/>')
+document.write('b = '+ b)
+document.write('<br/>')
+document.write('s = '+ s)
+document.write('<br/>')
